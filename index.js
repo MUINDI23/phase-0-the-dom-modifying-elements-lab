1f9dc8231cb76e217d0a8ecbaf4103cbc6d0c699
MUINDI23 <bbamnrb203915@spu.ac.ke>
2025-03-25 15:33:58 +0300

    Modify DOM

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+// Remove <main> element
+document.querySelector('main#main').remove();
+
+//create a new <h1> Element
+const newHeader = document.createElement("h1");
+
+//set id for <h1></h1>
+newHeader.id = "victory";
+
+//set content of newHeader
+newHeader.textContent = "Samuel is the champion";
+document.body.appendChild(newHeader);
